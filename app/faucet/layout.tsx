2ec6bf1deb5b21290f1a337154924c49537f07b8
emrestay <emrestay123@gmail.com>
2025-05-20 19:18:51 +0300

    retest

--- app/faucet/layout.tsx
+++
@@ -1,5 +0,0 @@
-import ClientLayout from '@/app/client-layout'
-
-export default function FaucetLayout({ children }: { children: React.ReactNode }) {
-  return <ClientLayout>{children}</ClientLayout>
-}
